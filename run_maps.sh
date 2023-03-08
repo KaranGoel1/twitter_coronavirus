d846d85678542773b1c8e20757be3b36d96dcfee
KaranGoel1 <kgoel24@cmc.edu>
2023-03-07 22:15:24 -0800

    fixed data and recreated plots

--- run_maps.sh
+++ run_maps.sh
@@ -1,2 +1,5 @@
+#!/bin/sh
+
 for file in /data/Twitter\ dataset/geoTwitter20-*;
-do nohup ./src/map.py --input_path="$file" & done
+do nohup ./src/map.py --input_path="$file" --output_folder='outputs_new' > outputcheck/$(basename "$file"|cut -f 1 -d '.') &
+done
